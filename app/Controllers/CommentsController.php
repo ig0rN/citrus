76416cca0b14187a5fda1f28606c71fa4781e395
Igor Nikolic <igornikolic77@yahoo.com>
2019-11-19 17:39:40 +0100

    Sync Front components with Models

--- app/Controllers/CommentsController.php
+++ app/Controllers/CommentsController.php
@@ -12,18 +12,18 @@ class CommentsController
      * Validate comment
      * Store comment in database
      */
-    public function store()
+    public function addComment()
     {
         $validation = ( new CommentRequest() )->validate($_POST);
 
         if (!$validation->passed()) {
-            App::get('session')->set('error', 'Validation failed. Try again.git a');
+            App::get('session')->set('error', 'Validation failed. Try again.');
             App::get('session')->set('errors', $validation->errors());
 
             return redirect('/');
         }
 
-        $result = ( new Comment() )->addComment($_POST);
+        $result = Comment::create($_POST);
 
         if ($result) {
             App::get('session')->set('success', 'You successfully send comment. Now you wait for admin to approve');
